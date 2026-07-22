68e86a4f26401742883a34301cf16a1579997d4f
MiguelSantizo <msantizo-2022021@kinal.edu.gt>
2026-07-22 10:23:07 -0600

    feat: se agregaron campos de titulo, ciudad, zona y direccion exacta al modelo de denuncia

--- CivicApp-BackEnd/src/models/denuncia.ts
+++ CivicApp-BackEnd/src/models/denuncia.ts
@@ -1,10 +1,14 @@
 export interface Denuncia {
   id_denuncia?: number;
   id_ciudadano: number;
-  id_categoria: number;
-  id_estado: number;
+  id_categoria: number; // 1-Baches, 2-Alumbrado/Electricidad, 3-Agua, 4-Ciudadano Problemático, 5-Basura
+  id_estado: number;    // 1-Pendiente, 2-En proceso, 3-Resuelto
+  titulo: string;       // Un título corto descriptivo
   descripcion: string;
-  latitud: number;
-  longitud: number;
+  ciudad: string;       // Ej: Guatemala, Mixco, Villa Nueva
+  zona: number;         // Ej: Zona 1, Zona 10, etc.
+  direccion_exacta?: string; // Opcional: Calle
+  latitud?: number;
+  longitud?: number;
   fecha_creacion?: Date;
 }
