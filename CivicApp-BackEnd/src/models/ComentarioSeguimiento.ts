d00749b7d1a5306839fcd092f916ca482cd1c8c8
MiguelSantizo <msantizo-2022021@kinal.edu.gt>
2026-07-22 11:43:04 -0600

    feat: se amplio para el seguimiento y nuevos datos de la empresa

---
+++ CivicApp-BackEnd/src/models/ComentarioSeguimiento.ts
@@ -0,0 +1,6 @@
+export interface ComentarioSeguimiento {
+  id_comentario: number;
+  autor: string; 
+  fecha: Date | string;
+  mensaje: string;
+}
--- CivicApp-BackEnd/src/models/denuncia.ts
+++ CivicApp-BackEnd/src/models/denuncia.ts
@@ -1,14 +1,15 @@
+import { ComentarioSeguimiento } from '../models/ComentarioSeguimiento';
+
 export interface Denuncia {
   id_denuncia?: number;
+  fecha_creacion: Date | string;
   id_ciudadano: number;
-  id_categoria: number; // 1-Baches, 2-Alumbrado/Electricidad, 3-Agua, 4-Ciudadano Problemático, 5-Basura
-  id_estado: number;    // 1-Pendiente, 2-En proceso, 3-Resuelto
-  titulo: string;       // Un título corto descriptivo
+  id_categoria: number;
+  id_estado: number; 
+  titulo: string;
   descripcion: string;
-  ciudad: string;       // Ej: Guatemala, Mixco, Villa Nueva
-  zona: number;         // Ej: Zona 1, Zona 10, etc.
-  direccion_exacta?: string; // Opcional: Calle
-  latitud?: number;
-  longitud?: number;
-  fecha_creacion?: Date;
+  ciudad: string;
+  zona: number;
+  direccion_exacta?: string;
+  comentarios?: ComentarioSeguimiento[]; 
 }
